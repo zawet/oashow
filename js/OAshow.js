db1be7dd1644a86bf39f21922c77c347236a6dca
zhangwentao <zhangwentao@qtone.cn>
2017-11-09 17:15:18 +0800

    取消环路切换，改善交互机制

--- js/OAshow.js
+++ js/OAshow.js
@@ -2,12 +2,15 @@ $(function () {
 
     var vl = new Swiper('.os-box', {
         direction: 'vertical',
-        loop: true,
-        lazyLoading : true,
-        onSlideChangeEnd: function (s) {
-            $(".os-box .swiper-slide .os-min").removeClass("animated").hide();
-            am($(".swiper-slide.swiper-slide-active").attr("ids"));
-        }
+        //loop: true,
+        //lazyLoading: true,
+        onTransitionStart: function(swiper){
+            am(swiper.activeIndex);
+		},
+		onTransitionEnd: function(swiper){
+            $(".os").eq(swiper.activeIndex).show().addClass("animated").siblings().find(".os-min").removeClass("animated").hide();
+            //console.log("end:"+swiper.activeIndex);
+   		}
     });
 
     $(".backlist").click(function () {
@@ -33,40 +36,39 @@ $(function () {
 
 });
 
-function am(id) {
-    //console.log(id);
-    $(".os-box .swiper-slide.swiper-slide-active .os-min").show().addClass("animated");
-    $(".os-box #office .swiper-slide-active .os-min").hide().removeClass("animated");
-    if(id=="index"||id==undefined){
-        $("#"+id+" .title1").addClass("fadeInLeft");
-        $("#"+id+" .title2").addClass("fadeInRight");
-        $("#"+id+" .mainpng").addClass("fadeIn").addClass("yc15");
-        $("#"+id+" .title3").addClass("fadeInUp").addClass("yc2");
+function am(si) {
+    $(".os.swiper-slide-active .os-min").show().addClass("animated");
+    var sid=$(".os.swiper-slide-active").attr("id");
+    //console.log(si,sid);
+    if (si == 0) {
+        $("#" + sid + " .title1").addClass("fadeInLeft");
+        $("#" + sid + " .title2").addClass("fadeInRight");
+        $("#" + sid + " .mainpng").addClass("fadeIn").addClass("yc15");
+        $("#" + sid + " .title3").addClass("fadeInUp").addClass("yc2");
     }
-    if(id=="list"){
-        $("#"+id+" .listtop").addClass("fadeIn");
-        $("#"+id+" .list-one").addClass("fadeInLeft");
-        $("#"+id+" .list-two").addClass("fadeInRight").addClass("yc15");
-        $("#"+id+" .list-three").addClass("fadeInLeft").addClass("yc2");
+    if (si == 1) {
+        $("#" + sid + " .listtop").addClass("fadeIn");
+        $("#" + sid + " .list-one").addClass("fadeInLeft");
+        $("#" + sid + " .list-two").addClass("fadeInRight").addClass("yc15");
+        $("#" + sid + " .list-three").addClass("fadeInLeft").addClass("yc2");
     }
-    if(id=="desk"||id=="commun"||id=="mail"||id=="doc"||id=="flow"){
-         $("#"+id+" .imgshow.sg").addClass("fadeInDown");
-         $("#"+id+" .imgshow.db").each(function(i){
-             $(this).find(".imgshow-bg").eq(0).addClass("fadeInLeft");
-             $(this).find(".imgshow-bg").eq(1).addClass("fadeInRight");
-         })
-         $("#"+id+" .imgtxt").addClass("flipInX").addClass("yc2");   
+    if (si >=2&&si<=6) {
+        $("#" + sid + " .imgshow.sg").addClass("fadeInDown");
+        $("#" + sid + " .imgshow.db").each(function (i) {
+            $(this).find(".imgshow-bg").eq(0).addClass("fadeInLeft");
+            $(this).find(".imgshow-bg").eq(1).addClass("fadeInRight");
+        })
+        $("#" + sid + " .imgtxt").addClass("flipInX").addClass("yc2");
     }
-    if(id=="office"){
-        $(".os-box #office .swiper-slide-active .os-min").show().addClass("animated");
-        $("#"+id+" .imgshow-bg").addClass("flipInY");
-        $("#"+id+" .imgtxt").addClass("flipInX").addClass("yc15");
+    if (si ==7) {
+        $(".os-box #office .swiper-slside-active .os-min").show().addClass("animated");
+        $("#" + sid + " .imgshow-bg").addClass("flipInY");
+        $("#" + sid + " .imgtxt").addClass("flipInX").addClass("yc15");
     }
-    if(id=="end"||id==undefined){
-        $("#"+id+" .endtitle").addClass("fadeInDown");
-        $("#"+id+" .endlogo").addClass("fadeInUp").addClass("yc20");
-        $("#"+id+" .endinfo").addClass("fadeInUp").addClass("yc30");
-
+    if (si == 8) {
+        $("#" + sid + " .endtitle").addClass("fadeInDown");
+        $("#" + sid + " .endlogo").addClass("fadeInUp").addClass("yc20");
+        $("#" + sid + " .endinfo").addClass("fadeInUp").addClass("yc30");
     }
 
 }
